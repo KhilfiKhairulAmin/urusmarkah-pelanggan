dc41c7b3cdb60d59f57d2d78df80996e884d62a8
iNFiENiTE-World <infienite@gmail.com>
2022-05-27 20:22:18 +0800

    Fungsi fail diterjemahkan semula. Kod difaktorkan untuk menyesuaikan konten fail dengan fungsinya

--- src/index.js
+++ src/index.js
@@ -1,38 +1,12 @@
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import SenaraiAkaun from './halaman/senarai_akaun';
-import DaftarMasuk from './halaman/daftar_masuk';
-import LogMasuk from './halaman/log_masuk';
-import PapanMaklumat from './halaman/pengguna/papan_maklumat';
-import SenaraiPertandingan from './halaman/pertandingan/senarai_pertandingan';
-import CiptaPertandingan from './halaman/pertandingan/cipta_pertandingan';
-import Pengesahan from './halaman/pengesahan';
 
 const container = document.getElementById('root');
 
 const root = ReactDOMClient.createRoot(container);
 
 root.render(
-  <BrowserRouter>
-    <Routes>
-
-      <Route path='/' element={<App />}>
-        <Route path='senarai' element={<SenaraiAkaun />} />
-      </Route>
-
-      <Route path='/pengesahan' element={<Pengesahan />}>
-        <Route path='daftar_masuk' element={<DaftarMasuk />} />
-        <Route path='log_masuk' element={<LogMasuk /> } />
-      </Route>
-
-      <Route path='/pengguna' element={<PapanMaklumat />} />
-
-      <Route path='/pertandingan' element={<SenaraiPertandingan />} />
-
-      <Route path='/pertandingan/cipta' element={<CiptaPertandingan />} />
-    </Routes>
-  </BrowserRouter>
+  <App />
 )
 
