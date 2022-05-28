e6e62e413952c236efb6cc2c7106d5337c70a9d9
iNFiENiTE-World <infienite@gmail.com>
2022-05-28 09:28:26 +0800

    Laman Utama dan Laman Pengesahan siap dibuat. Komponen identiti dibuat untuk logo sistem.

---
+++ src/komponen/identiti/Logo.js
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom"
+
+export default function Logo () {
+    return (
+        <Link to='/'><label style={{ color: 'orangered' }}>Urusmarkah</label></Link>
+    )
+}
